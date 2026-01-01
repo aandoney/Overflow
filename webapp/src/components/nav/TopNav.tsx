734b7651c92a2f96d475e28a6589f27234bfe2c7
aandoney <angel_andoney@live.com.mx>
2025-12-31 20:04:22 -0600

    added error handling and search

--- webapp/src/components/nav/TopNav.tsx
+++ webapp/src/components/nav/TopNav.tsx
@@ -4,6 +4,7 @@ import {Input} from "@heroui/input";
 import {MagnifyingGlassIcon} from "@heroicons/react/24/solid";
 import {Button} from "@heroui/button";
 import ThemeToggle from "@/components/nav/ThemeToggle";
+import SearchInput from "@/components/nav/SearchInput";
 
 export default function TopNav() {
     return (
@@ -20,12 +21,8 @@ export default function TopNav() {
                         <Link href='/'>Contact</Link>
                     </nav>
                 </div>
-                <Input
-                    startContent={<MagnifyingGlassIcon className='size-6' />}
-                    className='ml-6'
-                    type='search'
-                    placeholder='Search'
-                />
+
+                <SearchInput />
                 
                 <div className='flex basis-1/4 shrink-0 justify-end gap-3'>
                     <ThemeToggle />
